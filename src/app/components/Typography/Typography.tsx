5f92225d44f2ab1c75888534b388001f218de7ad
marwinburesch <marwin@posteo.de>
2021-08-24 17:20:49 +0200

    ✨ Add Typography component

---
+++ src/app/components/Typography/Typography.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './Typography.module.css';
+
+type TypographyProps = {
+  size: 'm' | 'l';
+  children: React.ReactNode;
+};
+
+export default function Typography({
+  size,
+  children,
+}: TypographyProps): JSX.Element {
+  if (size === 'm') {
+    return <h2>{children}</h2>;
+  } else {
+    return <h1>{children}</h1>;
+  }
+}
