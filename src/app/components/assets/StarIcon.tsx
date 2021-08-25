669e0c58f04869ed708f12bde68075d79c0037b6
mbantwerpes <marcel.blesing@antwerpes.com>
2021-08-25 11:03:21 +0200

    🐛  Spread starIconProps

--- src/app/components/assets/StarIcon.tsx
+++ src/app/components/assets/StarIcon.tsx
@@ -7,6 +7,7 @@ type StarIconProps = {
 const StarIcon = (
   props: React.SVGProps<SVGSVGElement> & StarIconProps
 ): JSX.Element => {
+  const { iconState, ...propsNoStarIconProps } = props;
   const iconStateMap = {
     active: (
       <linearGradient id="grad1">
@@ -35,8 +36,8 @@ const StarIcon = (
   };
 
   return (
-    <svg height="24" viewBox="0 0 24 24" width="24" {...props}>
-      <defs>{iconStateMap[props.iconState]}</defs>
+    <svg height="24" viewBox="0 0 24 24" width="24" {...propsNoStarIconProps}>
+      <defs>{iconStateMap[iconState]}</defs>
       <path
         fill="url(#grad1)"
         d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"
