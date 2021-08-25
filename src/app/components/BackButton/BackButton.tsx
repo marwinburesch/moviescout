d7c3b22125923e1d1ad8fe06a5a6bcf391cb97fb
RickKollenbroich93 <rick.kollenbroich93@googlemail.com>
2021-08-25 12:28:09 +0200

    ♻️ Change prop type and use BackButtonIcon component

--- src/app/components/BackButton/BackButton.tsx
+++ src/app/components/BackButton/BackButton.tsx
@@ -1,11 +1,18 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
+import BackButtonIcon from '../assets/BackButtonIcon';
 import styles from './BackButton.module.css';
 
 type BackButtonProps = {
-  children: ReactNode;
+  isHighlighted?: boolean;
 };
-function BackButton({ children }: BackButtonProps): JSX.Element {
-  return <button className={styles.backBtn}>{children}</button>;
+function BackButton({ isHighlighted }: BackButtonProps): JSX.Element {
+  return (
+    <button className={styles.backBtn}>
+      <BackButtonIcon
+        fill={isHighlighted ? 'var(--color-primary)' : 'var(--text-primary)'}
+      />
+    </button>
+  );
 }
 
 export default BackButton;
