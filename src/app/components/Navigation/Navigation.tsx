581932c267a71903be75f61df8d8c44b369bb8c9
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-08-25 16:32:20 +0200

    ✨ Create Navigation

---
+++ src/app/components/Navigation/Navigation.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import BookmarkIcon from '../assets/BookmarkIcon';
+import HomeIcon from '../assets/HomeIcon';
+import SearchIcon from '../assets/SearchIcon';
+import styles from './Navigation.module.css';
+
+function Navigation(): JSX.Element {
+  const [activeLink, setActiveLink] = useState<string>('home');
+  const active = {
+    fill: 'var(--color-primary)',
+    stroke: 'var(--color-primary)',
+  };
+
+  const inactive = {
+    fill: 'transparent',
+    stroke: 'var(--text-primary)',
+  };
+
+  return (
+    <nav className={styles.navigation}>
+      <NavLink
+        to="/"
+        isActive={(match) => {
+          if (match) {
+            setActiveLink('home');
+            return true;
+          }
+          return false;
+        }}
+      >
+        <HomeIcon
+          className={styles.home}
+          {...(activeLink === 'home' ? active : inactive)}
+        />
+      </NavLink>
+      <NavLink
+        to="/search"
+        isActive={(match) => {
+          if (match) {
+            setActiveLink('search');
+            return true;
+          }
+          return false;
+        }}
+      >
+        <SearchIcon
+          className={styles.search}
+          {...(activeLink === 'search' ? active : inactive)}
+        />
+      </NavLink>
+      <NavLink
+        to="/bookmark"
+        isActive={(match) => {
+          if (match) {
+            setActiveLink('bookmark');
+            return true;
+          }
+          return false;
+        }}
+      >
+        <BookmarkIcon
+          className={styles.bookmark}
+          {...(activeLink === 'bookmark' ? active : inactive)}
+        />
+      </NavLink>
+    </nav>
+  );
+}
+
+export default Navigation;
