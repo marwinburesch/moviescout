55dc68c7642e0b9714872c28395170a52c38d4a0
RickKollenbroich93 <rick.kollenbroich93@googlemail.com>
2021-08-25 15:06:20 +0200

    ✅  Add new class for styling by template strings

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react';
 import styles from './Header.module.css';
 import BackButton from '../BackButton/BackButton';
+import FlagButton from '../FlagButton/FlagButton';
 
 type HeaderProps = {
   children?: ReactNode;
@@ -14,12 +15,17 @@ function Header({
   isHighlighted = false,
 }: HeaderProps): JSX.Element {
   return (
-    <div className={styles.headerContainer}>
+    <div
+      className={`${styles.headerContainer} ${children ? '' : styles.flagGap}`}
+    >
       {withBackButton && <BackButton isHighlighted={isHighlighted} />}
-      <h1 className={styles.header}>
-        {children}
-        {children && <span className={styles.headerDot}>.</span>}
-      </h1>
+      {children && (
+        <h1 className={styles.header}>
+          {children}
+          <span className={styles.headerDot}>.</span>
+        </h1>
+      )}
+      {!children && <FlagButton />}
     </div>
   );
 }
