c327181297365b00ff73552bc850c3ee79aafe54
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-08-25 16:36:10 +0200

    🤡  Add story for navigation

---
+++ src/app/components/Navigation/Navigation.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from './Navigation';
+
+export default {
+  title: 'Component/Navigation',
+  component: Navigation,
+};
+
+export const NavigationDefault = (): JSX.Element => (
+  <Navigation/>
+);
+
