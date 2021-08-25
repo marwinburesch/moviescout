e950ddec8d9fc1a0a69bde476d6ee57cb1afe621
RickKollenbroich93 <rick.kollenbroich93@googlemail.com>
2021-08-25 12:25:07 +0200

    🏷️ Add new types for backbutton props ♻️ Header function

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -3,27 +3,22 @@ import styles from './Header.module.css';
 import BackButton from '../BackButton/BackButton';
 
 type HeaderProps = {
-  children: ReactNode | null;
+  children?: ReactNode;
+  withBackButton?: boolean;
+  isHighlighted?: boolean;
 };
 
-function Header({ children }: HeaderProps): JSX.Element {
-  let backImage: string;
-  if (children !== ' ') {
-    backImage = 'src/assets/back.svg';
-  } else {
-    backImage = 'src/assets/backWhite.svg';
-  }
-
+function Header({
+  children,
+  withBackButton = false,
+  isHighlighted = false,
+}: HeaderProps): JSX.Element {
   return (
     <div className={styles.headerContainer}>
-      {children && children !== 'TopFive' && (
-        <BackButton>{<img src={backImage} />}</BackButton>
-      )}
+      {withBackButton && <BackButton isHighlighted={isHighlighted} />}
       <h1 className={styles.header}>
         {children}
-        {children && children !== ' ' && (
-          <span className={styles.headerDot}>.</span>
-        )}
+        {children && <span className={styles.headerDot}>.</span>}
       </h1>
     </div>
   );
