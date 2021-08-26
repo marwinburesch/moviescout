5b1b4985a9db44812570a36a47e93c71a878d039
mbantwerpes <marcel.blesing@antwerpes.com>
2021-08-26 11:00:19 +0200

    Add chevron icon

---
+++ src/app/components/assets/ChevronRightIcon.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function FlagIcon(
+  props: React.SVGProps<SVGSVGElement>
+): JSX.Element {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      {...props}
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M13 5l7 7-7 7M5 5l7 7-7 7"
+      />
+    </svg>
+  );
+}
