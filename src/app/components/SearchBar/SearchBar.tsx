1b043d95064cc779fd8dd2164bf1d566b00d65f7
Seborider <mayer.sebobo@gmail.com>
2021-08-26 15:38:06 +0200

    🏷️  Add types for SearchBarProps

--- src/app/components/SearchBar/SearchBar.tsx
+++ src/app/components/SearchBar/SearchBar.tsx
@@ -2,17 +2,29 @@ import React from 'react';
 import styles from './SearchBar.module.css';
 import SearchIcon from '../assets/SearchIcon';
 
-function SearchBar(): JSX.Element {
+type SearchBarProps = {
+  searchResult: string;
+  handleSubmit: () => void;
+  setSearchResult: (searchResult: string) => void;
+};
+
+function SearchBar({
+  searchResult,
+  handleSubmit,
+  setSearchResult,
+}: SearchBarProps): JSX.Element {
   return (
-    <div className={styles.container}>
+    <form className={styles.container} onSubmit={handleSubmit}>
       <SearchIcon fill="white" width="12" height="12" />
 
       <input
         type="search"
         placeholder="Search"
         className={styles.searchBar__input}
+        value={searchResult}
+        onChange={(event) => setSearchResult(event.target.value)}
       />
-    </div>
+    </form>
   );
 }
 
