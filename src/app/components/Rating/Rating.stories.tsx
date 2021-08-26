866bf9f60e50fea53eb1758b1c4877d3c6954126
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-08-26 16:19:05 +0200

    🤡 Add more examples for storiebook

--- src/app/components/Rating/Rating.stories.tsx
+++ src/app/components/Rating/Rating.stories.tsx
@@ -4,5 +4,24 @@ import Rating from './Rating';
 export default {
   title: 'Component/Rating',
   component: Rating,
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+    },
+  },
 };
-export const RatingStars = (): JSX.Element => <Rating />;
+export const RatingStars = (): JSX.Element => (
+  <Rating rating={3.5} maxRating={5} />
+);
+
+export const RatingFiveStars = (): JSX.Element => (
+  <Rating rating={5} maxRating={5} />
+);
+
+export const RatingGreaterStars = (): JSX.Element => (
+  <Rating rating={8.5} maxRating={10} />
+);
+
+export const RatingSingelStar = (): JSX.Element => (
+  <Rating rating={3.5} maxRating={1} />
+);
