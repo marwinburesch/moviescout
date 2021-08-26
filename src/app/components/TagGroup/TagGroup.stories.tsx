d7b8352613dea484bd99a3103c8c07d803be758f
mbantwerpes <marcel.blesing@antwerpes.com>
2021-08-26 10:37:18 +0200

    Add tag group story

---
+++ src/app/components/TagGroup/TagGroup.stories.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import TagGroup from './TagGroup';
+import { Story } from '@storybook/react';
+import type { TagGroupProps } from './TagGroup';
+
+export default {
+  title: 'Component/TagGroup',
+  component: TagGroup,
+  argTypes: {},
+};
+
+const mockFunction = () => console.log('📽');
+
+const tags = [
+  {
+    count: '',
+    active: false,
+    onClick: mockFunction,
+    children: 'all',
+  },
+  {
+    count: '4',
+    active: false,
+    onClick: mockFunction,
+    children: 'animation',
+  },
+  {
+    count: '6',
+    active: true,
+    onClick: mockFunction,
+    children: 'action',
+  },
+  {
+    count: '9',
+    active: false,
+    onClick: mockFunction,
+    children: 'comedy',
+  },
+  {
+    count: '7',
+    active: false,
+    onClick: mockFunction,
+    children: 'sci fi',
+  },
+];
+
+const Template: Story<TagGroupProps> = (args) => <TagGroup {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  tagList: tags,
+};
