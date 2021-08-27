dc0f5c2d2dfcc05afa1e4856b613acfe9ea95826
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-27 10:55:21 +0200

    Cards (#22)
    * ✨ Setup card and types
    
    * 🤡 Add story
    
    * 🎨 Add basic  HTML structure
    
    * 💄 Style card component
    
    * 💄 Style  detail card and wide card
    
    * Create stories for wide card, detail card, compact card, test card
    
    * Add conditional  rendering
    
    * Remove unused imports
    
    * Add dark background to storybook
    
    * Rename div to section
    
    * Add new story with a longer title and longer gentres

---
+++ src/app/components/Card/Card.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import MovieButton from '../MovieButton/MovieButton';
+import RatingIcon from '../RatingIcon/RatingIcon';
+import styles from './Card.module.css';
+
+export type CardProps = {
+  layout: 'detail' | 'compact' | 'wide';
+  title: string;
+  rating: number | null;
+  genres: string[];
+  image: string | null;
+  isBookmarked?: boolean;
+  onBookmarkClick: () => void;
+  children: React.ReactNode;
+};
+
+export default function Card({
+  layout,
+  title,
+  rating,
+  genres,
+  image,
+  isBookmarked = false,
+  onBookmarkClick,
+  children,
+}: CardProps): JSX.Element {
+  return (
+    <article className={`${styles.article} ${styles[`article--${layout}`]}`}>
+      <div
+        className={styles.image}
+        style={{ backgroundImage: `url(${image})` }}
+      >
+        <div className={styles.bookmark}>
+          <MovieButton
+            iconType="bookmark"
+            isActive={isBookmarked}
+            onButtonClick={onBookmarkClick}
+          ></MovieButton>
+        </div>
+      </div>
+      <section>
+        <p className={styles.title}>{title}</p>
+        <div>
+          {rating}
+          <RatingIcon iconState="active" />
+          <RatingIcon iconState="active" />
+          <RatingIcon iconState="active" />
+          <RatingIcon iconState="active" />
+          <RatingIcon iconState="active" />
+        </div>
+        {layout === 'detail' && (
+          <>
+            <p className={styles.genres}>{genres.join(', ')}</p>
+
+            <p className={styles.description}>{children}</p>
+          </>
+        )}
+      </section>
+    </article>
+  );
+}
