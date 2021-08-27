edf8b8a0e069e7b19c5bf81a911d2c12d443c70f
christophschmitz <c.schmitz@outlook.de>
2021-08-27 13:26:15 +0200

    ✨  Add card component

--- src/app/pages/Discover/Discover.tsx
+++ src/app/pages/Discover/Discover.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './Discover.module.css';
 import Header from '../../components/Header/Header';
+import Card from '../../components/Card/Card';
 
 export default function Discover(): JSX.Element {
   return (
@@ -8,6 +9,88 @@ export default function Discover(): JSX.Element {
       <Header withBackButton isHighlighted>
         Discover
       </Header>
+      <div className={styles.cardWrapper}>
+        <Card
+          layout="compact"
+          title="Real long Movie titel number 1"
+          genres={['']}
+          image={null}
+          onBookmarkClick={() => console.log('')}
+          rating={3.5}
+        >
+          Lorem ipsum
+        </Card>
+        <Card
+          layout="compact"
+          title="Addams Family"
+          genres={['']}
+          image={null}
+          onBookmarkClick={() => console.log('')}
+          rating={3.5}
+        >
+          Lorem ipsum
+        </Card>
+        <Card
+          layout="compact"
+          title=""
+          genres={['']}
+          image={null}
+          onBookmarkClick={() => console.log('')}
+          rating={3.5}
+        >
+          Lorem ipsum
+        </Card>
+        <Card
+          layout="compact"
+          title=""
+          genres={['']}
+          image={null}
+          onBookmarkClick={() => console.log('')}
+          rating={3.5}
+        >
+          Lorem ipsum
+        </Card>
+        <Card
+          layout="compact"
+          title=""
+          genres={['']}
+          image={null}
+          onBookmarkClick={() => console.log('')}
+          rating={3.5}
+        >
+          Lorem ipsum
+        </Card>
+        <Card
+          layout="compact"
+          title=""
+          genres={['']}
+          image={null}
+          onBookmarkClick={() => console.log('')}
+          rating={3.5}
+        >
+          Lorem ipsum
+        </Card>
+        <Card
+          layout="compact"
+          title=""
+          genres={['']}
+          image={null}
+          onBookmarkClick={() => console.log('')}
+          rating={3.5}
+        >
+          Lorem ipsum
+        </Card>
+        <Card
+          layout="compact"
+          title=""
+          genres={['']}
+          image={null}
+          onBookmarkClick={() => console.log('')}
+          rating={3.5}
+        >
+          Lorem ipsum
+        </Card>
+      </div>
     </main>
   );
 }
