496a849e94b72e5745e43b3a38e8ca19e15a289b
phlgr <philipp@gartz.dev>
2021-08-27 17:06:49 +0200

    ✨ Implement getPopularMovies and getMovie functionality

---
+++ src/lib/models/movie.ts
@@ -0,0 +1,45 @@
+import fetch from 'node-fetch';
+import { GENRES } from '../genreMap';
+import {
+  GenreIDs,
+  Movie,
+  MovieDetailsFromAPI,
+  MovieFromAPI,
+  MoviesFromAPI,
+} from '../types';
+const API_KEY = process.env.API_KEY;
+
+export async function getPopularMovies(): Promise<Movie[]> {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US`
+  );
+  const movies: MoviesFromAPI = await response.json();
+  const formattedMovies = movies.results.map((movie: MovieFromAPI) => {
+    return {
+      title: movie.title,
+      rating: movie.vote_average,
+      overview: movie.overview,
+      image: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      id: movie.id,
+      genres: movie.genre_ids.map((id: GenreIDs) =>
+        GENRES[id] ? GENRES[id] : ''
+      ),
+    };
+  });
+  return formattedMovies;
+}
+
+export async function getMovie(id: string): Promise<Movie> {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`
+  );
+  const movie: MovieDetailsFromAPI = await response.json();
+  return {
+    title: movie.title,
+    rating: movie.vote_average,
+    overview: movie.overview,
+    image: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+    id: movie.id,
+    genres: movie.genres?.map((genre) => genre.name),
+  };
+}
