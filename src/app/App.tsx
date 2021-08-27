6942f24da13c14d691f29d14e8683a0bc615fbfa
Seborider <mayer.sebobo@gmail.com>
2021-08-27 16:55:45 +0200

    ✨  Add /bookmark route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Bookmarks from './pages/Bookmarks/Bookmarks';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/bookmark">
+          <Bookmarks />
+        </Route>
         <Route path="/about">
           <main>About</main>
         </Route>
