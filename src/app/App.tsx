d90d927ff37d5c4b8de90a7830a85f13aa46abe2
christophschmitz <c.schmitz@outlook.de>
2021-08-27 11:28:25 +0200

    🚚  Add route to app tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Discover from './pages/Discover/Discover';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/discover">
+          <Discover />
+        </Route>
         <Route path="/about">
           <main>About</main>
         </Route>
