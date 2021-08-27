6daa96fd969e5c2b86ae802bff7c659d21e4ac98
christophschmitz <c.schmitz@outlook.de>
2021-08-27 15:15:41 +0200

    🤡  Mock movies to discover page

--- src/app/pages/Discover/Discover.tsx
+++ src/app/pages/Discover/Discover.tsx
@@ -1,8 +1,119 @@
 import React from 'react';
 import styles from './Discover.module.css';
 import Header from '../../components/Header/Header';
-import Card from '../../components/Card/Card';
+import Card, { CardProps } from '../../components/Card/Card';
 import Navigation from '../../components/Navigation/Navigation';
+import TagGroup from '../../components/TagGroup/TagGroup';
+
+const mockFunction = () => console.log('📽');
+
+const tags = [
+  {
+    count: '',
+    active: false,
+    onClick: mockFunction,
+    children: 'all',
+  },
+  {
+    count: '4',
+    active: false,
+    onClick: mockFunction,
+    children: 'animation',
+  },
+  {
+    count: '6',
+    active: true,
+    onClick: mockFunction,
+    children: 'action',
+  },
+  {
+    count: '9',
+    active: false,
+    onClick: mockFunction,
+    children: 'comedy',
+  },
+  {
+    count: '7',
+    active: false,
+    onClick: mockFunction,
+    children: 'sci fi',
+  },
+];
+
+const mockMovies: CardProps[] = [
+  {
+    layout: 'compact',
+    title: 'Real long Movie Title Number 1',
+    genres: [''],
+    image: null,
+    onBookmarkClick: () => console.log(''),
+    rating: 3.5,
+    children: 'bliblablu',
+  },
+  {
+    layout: 'compact',
+    title: 'Real long Movie Title Number 1',
+    genres: [''],
+    image: null,
+    onBookmarkClick: () => console.log(''),
+    rating: 3.5,
+    children: 'bliblablu',
+  },
+  {
+    layout: 'compact',
+    title: 'Real long Movie Title Number 1',
+    genres: [''],
+    image: null,
+    onBookmarkClick: () => console.log(''),
+    rating: 3.5,
+    children: 'bliblablu',
+  },
+  {
+    layout: 'compact',
+    title: 'Real long Movie Title Number 1',
+    genres: [''],
+    image: null,
+    onBookmarkClick: () => console.log(''),
+    rating: 3.5,
+    children: 'bliblablu',
+  },
+  {
+    layout: 'compact',
+    title: 'Real long Movie Title Number 1',
+    genres: [''],
+    image: null,
+    onBookmarkClick: () => console.log(''),
+    rating: 3.5,
+    children: 'bliblablu',
+  },
+  {
+    layout: 'compact',
+    title: 'Real long Movie Title Number 1',
+    genres: [''],
+    image: null,
+    onBookmarkClick: () => console.log(''),
+    rating: 3.5,
+    children: 'bliblablu',
+  },
+  {
+    layout: 'compact',
+    title: 'Real long Movie Title Number 1',
+    genres: [''],
+    image: null,
+    onBookmarkClick: () => console.log(''),
+    rating: 3.5,
+    children: 'bliblablu',
+  },
+  {
+    layout: 'compact',
+    title: 'Real long Movie Title Number 1',
+    genres: [''],
+    image: null,
+    onBookmarkClick: () => console.log(''),
+    rating: 3.5,
+    children: 'bliblablu',
+  },
+];
 
 export default function Discover(): JSX.Element {
   return (
@@ -10,91 +121,13 @@ export default function Discover(): JSX.Element {
       <Header withBackButton isHighlighted>
         Discover
       </Header>
-
+      <TagGroup tagList={tags} />
       <div className={styles.cardWrapper}>
-        <Card
-          layout="compact"
-          title="Real long Movie Title Number 1"
-          genres={['']}
-          image={null}
-          onBookmarkClick={() => console.log('')}
-          rating={3.5}
-        >
-          Lorem ipsum
-        </Card>
-        <Card
-          layout="compact"
-          title="Addams Family - Reunion"
-          genres={['']}
-          image={null}
-          onBookmarkClick={() => console.log('')}
-          rating={3.5}
-        >
-          Lorem ipsum
-        </Card>
-        <Card
-          layout="compact"
-          title=""
-          genres={['']}
-          image={null}
-          onBookmarkClick={() => console.log('')}
-          rating={3.5}
-        >
-          Lorem ipsum
-        </Card>
-        <Card
-          layout="compact"
-          title=""
-          genres={['']}
-          image={null}
-          onBookmarkClick={() => console.log('')}
-          rating={3.5}
-        >
-          Lorem ipsum
-        </Card>
-        <Card
-          layout="compact"
-          title=""
-          genres={['']}
-          image={null}
-          onBookmarkClick={() => console.log('')}
-          rating={3.5}
-        >
-          Lorem ipsum
-        </Card>
-        <Card
-          layout="compact"
-          title=""
-          genres={['']}
-          image={null}
-          onBookmarkClick={() => console.log('')}
-          rating={3.5}
-        >
-          Lorem ipsum
-        </Card>
-        <Card
-          layout="compact"
-          title=""
-          genres={['']}
-          image={null}
-          onBookmarkClick={() => console.log('')}
-          rating={3.5}
-        >
-          Lorem ipsum
-        </Card>
-        <Card
-          layout="compact"
-          title=""
-          genres={['']}
-          image={null}
-          onBookmarkClick={() => console.log('')}
-          rating={3.5}
-        >
-          Lorem ipsum
-        </Card>
+        {mockMovies.length !== 0 &&
+          mockMovies.map((movie) => <Card {...movie} />)}
       </div>
       <div className={styles.navigation}>
-        <Navigation />
+        <Navigation activeLink="home" />
       </div>
     </main>
   );
