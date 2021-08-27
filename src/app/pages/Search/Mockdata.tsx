ec4178db1782f7e7e550b9826ae093f8680d98e3
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-27 16:54:12 +0200

    Create mocks data

---
+++ src/app/pages/Search/Mockdata.tsx
@@ -0,0 +1,15 @@
+export const mockTagGroupProps = [
+  { children: 'Animation', count: '0' },
+  { children: 'Action', count: '3', active: true },
+  { children: 'Comedy', count: '1' },
+];
+
+export const mockCard = {
+  genres: ['Comedy', 'Action', 'Love Film'],
+  title: 'Pulp Fiction 7',
+  rating: 4.5,
+  image:
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHILF70VeqhsH_fzgLs2X_5X_zP1Q6_tuEqLeo8p_vIsfeigYK4xqyvWI5pzYZZ2Hf2Bk&usqp=CAU',
+  children:
+    'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti, expedita repudiandae numquam quidem obcaecati aliquid id sapiente architecto et incidunt pariatur delectus facere dolor earum iusto suscipit ratione. Officia ducimus aspernatur incidunt ea eligendi nobis dolorum ab quasi, nostrum harum officiis suscipit voluptates necessitatibus nemo debitis architecto, facere in natus laborum! A aspernatur maiores magnam. Nihil expedita in dolorum quibusdam quam assumenda, voluptatem vitae accusamus beatae laudantium incidunt qui laboriosam porro rem ad totam, quasi molestias error quod earum ipsa recusandae itaque velit consectetur. Ab reiciendis labore dignissimos incidunt officiis similique, veniam ipsam cumque aperiam a amet harum minus accusamus!',
+};
