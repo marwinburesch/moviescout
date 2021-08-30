8d2cf8eea9362135e4a69030a88641ce756754c8
phlgr <philipp@gartz.dev>
2021-08-30 15:14:16 +0200

    ✨ Add latest route

--- src/lib/models/movie.ts
+++ src/lib/models/movie.ts
@@ -29,11 +29,37 @@ export async function getPopularMovies(): Promise<Movie[]> {
   return formattedMovies;
 }
 
+export async function getLatestMovies(): Promise<Movie[]> {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&region=DE`
+  );
+  const movies: MoviesFromAPI = await response.json();
+  const formattedMovies = movies.results.map((movie: MovieFromAPI) => {
+    return {
+      title: movie.title,
+      rating: movie.vote_average,
+      overview: movie.overview,
+      image: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      id: movie.id,
+      genres: movie.genre_ids.map((id: GenreIDs) =>
+        GENRES[id] ? GENRES[id] : ''
+      ),
+    };
+  });
+  return formattedMovies;
+}
+
 export async function getMovie(id: string): Promise<Movie> {
   const response = await fetch(
     `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`
   );
+
   const movie: MovieDetailsFromAPI = await response.json();
+
+  if (!movie.title) {
+    throw new Error('Movie not found');
+  }
+
   return {
     title: movie.title,
     rating: movie.vote_average,
--- src/lib/routes/movie.ts
+++ src/lib/routes/movie.ts
@@ -1,17 +1,30 @@
 import { Router } from 'express';
-import { getMovie, getPopularMovies } from '../models/movie';
+import { getLatestMovies, getMovie, getPopularMovies } from '../models/movie';
 
 const router = Router();
 
 router.get('/popular', async (_req, res) => {
-  const popularMovies = await getPopularMovies();
-  res.json(popularMovies);
+  try {
+    const popularMovies = await getPopularMovies();
+    res.json(popularMovies);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.get('/latest', async (_req, res) => {
+  const latestMovies = await getLatestMovies();
+  res.json(latestMovies);
 });
 
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
-  const movie = await getMovie(id);
-  res.json(movie);
+  try {
+    const movie = await getMovie(id);
+    res.json(movie);
+  } catch (err) {
+    res.status(404).json({ error: 'Movie not found' });
+  }
 });
 
 export default router;
