60fa3c6a31820763bf11d957f036c9de22bbc2d0
ChristianSeelemann <cseelemann@gmx.de>
2021-08-30 09:47:12 +0200

    Fix not used import

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import MovieButton from '../MovieButton/MovieButton';
 import Rating from '../Rating/Rating';
-import RatingIcon from '../RatingIcon/RatingIcon';
 import styles from './Card.module.css';
 
 export type CardProps = {
