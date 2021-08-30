b86b74340c5312633910fb9783615f26d033c304
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-30 12:39:45 +0200

    🎨 Delete section from main

--- src/app/pages/Search/Search.tsx
+++ src/app/pages/Search/Search.tsx
@@ -18,51 +18,47 @@ export default function Search(): JSX.Element {
       <Header withBackButton isHighlighted>
         Search
       </Header>
+      <SearchBar
+        searchResult={searchValue}
+        setSearchResult={setSearchValue}
+        handleSubmit={handleSubmit}
+        className={styles.searchbar}
+      />
       <section className={styles.search}>
-        <div className={styles.searchbar}>
-          <SearchBar
-            searchResult={searchValue}
-            setSearchResult={setSearchValue}
-            handleSubmit={handleSubmit}
-          />
-        </div>
-
         <TagGroup tagList={mockTagGroupProps} />
         <p>Search Results(3)</p>
       </section>
-      <main>
-        <section className={styles.cards}>
-          <Card
-            title={mockCard.title}
-            rating={mockCard.rating}
-            image={mockCard.image}
-            layout="detail"
-            genres={mockCard.genres}
-            onBookmarkClick={() => console.log('hallo')}
-          >
-            {mockCard.children}
-          </Card>
-          <Card
-            title={mockCard.title}
-            rating={mockCard.rating}
-            image={mockCard.image}
-            layout="detail"
-            genres={mockCard.genres}
-            onBookmarkClick={() => console.log('hallo')}
-          >
-            {mockCard.children}
-          </Card>
-          <Card
-            title={mockCard.title}
-            rating={mockCard.rating}
-            image={mockCard.image}
-            layout="detail"
-            genres={mockCard.genres}
-            onBookmarkClick={() => console.log('hallo')}
-          >
-            {mockCard.children}
-          </Card>
-        </section>
+      <main className={styles.cards}>
+        <Card
+          title={mockCard.title}
+          rating={mockCard.rating}
+          image={mockCard.image}
+          layout="detail"
+          genres={mockCard.genres}
+          onBookmarkClick={() => console.log('hallo')}
+        >
+          {mockCard.children}
+        </Card>
+        <Card
+          title={mockCard.title}
+          rating={mockCard.rating}
+          image={mockCard.image}
+          layout="detail"
+          genres={mockCard.genres}
+          onBookmarkClick={() => console.log('hallo')}
+        >
+          {mockCard.children}
+        </Card>
+        <Card
+          title={mockCard.title}
+          rating={mockCard.rating}
+          image={mockCard.image}
+          layout="detail"
+          genres={mockCard.genres}
+          onBookmarkClick={() => console.log('hallo')}
+        >
+          {mockCard.children}
+        </Card>
       </main>
       <Navigation activeLink="search" />
     </div>
