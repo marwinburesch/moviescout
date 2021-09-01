d2a70e157cdc235c247d99777b4721e8025f5df2
Christoph Schmitz <87307464+christophschmitz@users.noreply.github.com>
2021-09-01 09:56:19 +0200

    Discoverpage (#26)
    * 🚚  Add route to app tsx
    
    * ✨  Import of Header component
    
    * ✨  Add card component
    
    * 💄 Add basic styling of discover page
    
    * 🐛  Fix props in navigation component
    
    * ✨  Add navigation component
    
    * 💄  Add styling to navigation
    
    * 🤡  Mock movies to discover page
    
    * 🤡 Mock our Cards
    
    * 💄 Finalize styling
    
    * 🔥 Remove unnecessary code
    
    * 💄 Slimify css code
    
    * 💄  Align heading with backbutton
    
    * 💄 Add responsive layout to discover page
    
    * ✨ Add onBookmarkClick function to card component
    
    * 🏷️  Refactore types

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Home from './pages/Home/Home';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Discover from './pages/Discover/Discover';
 import Bookmarks from './pages/Bookmarks/Bookmarks';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/discover">
+          <Discover />
+        </Route>
         <Route path="/bookmark">
           <Bookmarks />
         </Route>
