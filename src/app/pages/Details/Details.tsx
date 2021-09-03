b502761f00557649811285ce8bb33f5c45858b58
Christian Seelemann <64561043+ChristianSeelemann@users.noreply.github.com>
2021-09-03 16:02:20 +0200

    Add movie data and readmore functionality to details page (#50)
    * ✨ Create useFetch hook
    
    * ✨ Create useMovieDetails hook
    
    * 🤡  Mock details page with API data
    
    * 💄  Add whitespace to ActionLink
    
    * 💄  Add styling for read more button
    
    * ✨  Add logic for read more button

--- src/app/pages/Details/Details.tsx
+++ src/app/pages/Details/Details.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../components/Header/Header';
 import Navigation from '../../components/Navigation/Navigation';
 import styles from './Details.module.css';
+import useMovieDetails from '../../hooks/useMovieDetails';
+import { useParams } from 'react-router';
+import ActionLink from '../../components/ActionLink/ActionLink';
 
 export default function Details(): JSX.Element {
+  const { id }: { id: string } = useParams();
+  const { movie } = useMovieDetails(id);
+  const [readMore, setReadMore] = useState(false);
+
+  function handleReadMore() {
+    setReadMore(!readMore);
+  }
+
   return (
     <main className={styles.container}>
       <header className={styles.header}>
@@ -11,20 +22,21 @@ export default function Details(): JSX.Element {
       </header>
 
       <section className={styles.details}>
-        <img
-          className={styles.image}
-          src="https://image.tmdb.org/t/p/w500/kb4s0ML0iVZlG6wAKbbs9NAm6X.jpg"
-        />
+        <img className={styles.image} src={movie?.image} />
         <div className={styles.info}>
-          <h2 className={styles.title}>The Suicide Squad</h2>
-          <p className={styles.description}>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta
-            dignissimos vel, nemo ullam odio quos nostrum doloremque molestias
-            cum eius laborum consectetur quaerat, incidunt laudantium impedit
-            vero omnis. Autem error suscipit magnam aspernatur ad iure voluptas
-            ipsa deleniti culpa nulla!
+          <h2 className={styles.title}>{movie?.title}</h2>
+          <p className={!readMore ? styles.description : styles.readMore}>
+            {movie?.overview}
           </p>
-          <span className={styles.ctaText}>Read more</span>
+          <span onClick={handleReadMore} className={styles.ctaText}>
+            {!readMore ? 'Read more' : 'Read less'}
+          </span>
+          <ActionLink
+            children="See on The Movie DB"
+            url={`https://www.themoviedb.org/movie/${id}`}
+            hasIcon={false}
+            className={styles.actionLink}
+          />
         </div>
       </section>
 
