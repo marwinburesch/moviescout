b39e144aec3c722055d63ecc22a91d47ebbd4a4d
marwinburesch <marwin@posteo.de>
2021-09-06 12:01:17 +0200

    ♻️ Change index to value for key prop

--- src/app/components/Rating/Rating.tsx
+++ src/app/components/Rating/Rating.tsx
@@ -13,21 +13,19 @@ function Rating({
     <section className={`${styles.rating} ${className}`}>
       <p className={styles.ratingValue}>{rating}</p>
       <div className={styles.starContainer}>
-        {Array.from({ length: maxRating }, (_, i) => i + 1).map(
-          (value, index) => {
-            let starStatus: 'active' | 'inactive' | 'halfActive';
-            if (value < rating + 1) {
-              if (rating % 1 === 0 || rating > value) {
-                starStatus = 'active';
-              } else {
-                starStatus = 'halfActive';
-              }
+        {Array.from({ length: maxRating }, (_, i) => i + 1).map((value) => {
+          let starStatus: 'active' | 'inactive' | 'halfActive';
+          if (value < rating + 1) {
+            if (rating % 1 === 0 || rating > value) {
+              starStatus = 'active';
             } else {
-              starStatus = 'inactive';
+              starStatus = 'halfActive';
             }
-            return <RatingIcon iconState={starStatus} key={index} />;
+          } else {
+            starStatus = 'inactive';
           }
-        )}
+          return <RatingIcon iconState={starStatus} key={value} />;
+        })}
       </div>
     </section>
   );
