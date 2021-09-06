4bc73984e46e6c5ac76c494dcf332a810838a265
marwinburesch <marwin@posteo.de>
2021-09-06 13:57:07 +0200

    ♻️ Extract function tp generate number list

--- src/app/components/Rating/Rating.tsx
+++ src/app/components/Rating/Rating.tsx
@@ -9,11 +9,15 @@ function Rating({
   maxRating = 5,
   className,
 }: RatingProps): JSX.Element {
+  function createRangeListFrom(number: number): number[] {
+    return Array.from({ length: number }, (_, i) => i + 1);
+  }
+
   return (
     <section className={`${styles.rating} ${className}`}>
       <p className={styles.ratingValue}>{rating}</p>
       <div className={styles.starContainer}>
-        {Array.from({ length: maxRating }, (_, i) => i + 1).map((value) => {
+        {createRangeListFrom(maxRating).map((value) => {
           let starStatus: 'active' | 'inactive' | 'halfActive';
           if (value < rating + 1) {
             if (rating % 1 === 0 || rating > value) {
