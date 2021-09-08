5a1779bb99d5f7c3f0e0f89f839a3dcfd9924329
marwinburesch <marwin@posteo.de>
2021-09-08 10:44:42 +0200

    🔥 Remove unused import

--- src/app/pages/Home/Home.tsx
+++ src/app/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Header from '../../components/Header/Header';
 import Card from '../../components/Card/Card';
 import Navigation from '../../components/Navigation/Navigation';
